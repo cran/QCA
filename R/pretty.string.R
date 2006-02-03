1b92d56335e01817d01da385e1097929431384f4
Adrian Dusa <adi@roda.ro>
2006-02-03 00:00:00 +0000

    version 0.1-1

--- R/pretty.string.R
+++ R/pretty.string.R
@@ -1,16 +1,16 @@
 "pretty.string" <-
-function(string.vector, string.width=60, repeat.space=5, separator=";") { 
+function(string.vector, string.width=60, repeat.space=5, separator=",") { 
     if (length(string.vector) > 1) {
         string <- string.vector[1]
-        startpoint <- 2
+        startpoint <- 1
         for (j in 2:length(string.vector)) {
-            if (nchar(paste(string.vector[startpoint:j], collapse=separator)) < string.width) {
-                string <- paste(string, string.vector[j], sep=separator)
-            } else {
+            if (nchar(paste(string.vector[startpoint:j], collapse=separator)) > string.width) {
                 string <- paste(paste(string, "\n", sep=separator), 
                                 paste(rep(" ", repeat.space), collapse=""), 
                                 string.vector[j], sep="")
                 startpoint <- j
+            } else {
+                string <- paste(string, string.vector[j], sep=separator)
             }
         }
         string
