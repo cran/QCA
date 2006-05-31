61f86c07ce0e7a71a8e4ab83009027db28eadcb5
Adrian Dusa <adi@roda.ro>
2006-05-31 00:00:00 +0000

    version 0.2-0

--- R/qmcc.R
+++ R/qmcc.R
@@ -2,8 +2,8 @@
 function(mydata, outcome = "", conditions = c(""), incl.rem = FALSE,
          expl.1 = FALSE, expl.0 = FALSE, expl.ctr = FALSE, expl.mo = FALSE,
          incl.1 = FALSE, incl.0 = FALSE, incl.ctr = FALSE, incl.mo = FALSE,
-         quiet = FALSE, details = FALSE, chart = FALSE,
-         use.letters = TRUE, show.cases = FALSE, tt = FALSE) {
+         quiet = FALSE, details = FALSE, chart = FALSE, use.letters = TRUE,
+         show.cases = FALSE, tt = FALSE) {
 
      # check if the data has column names
     if (is.null(colnames(mydata))) {
